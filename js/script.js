3c2535dbac28ca726424c9f18664e7c77041f189
ANNA Skowronek-Jaskuła <annaskowronek-jaskula@MacBook-Air-ANNA.local>
2017-09-04 14:40:31 +0200

    Rev.2

--- js/script.js
+++ js/script.js
@@ -18,40 +18,42 @@ phone.printInfo(); // price = 1000
 phone.applyDiscount(100); // nic się nie wypisuje
 phone.printInfo(); // price = 900
 */
-function Phone(brand, price, color, img, brandShortcut) {
+function Phone(brand, price, color, img, idBrandShortcut) {
 	this.brand = brand;
 	this.price = price;
 	this.color = color;
 	this.img = img;
-	this.brandShortcut = brandShortcut;
-
+	this.idBrandShortcut = idBrandShortcut;
 }
 
-	
+function capitaliseFirstLetterSentence(sentence) {
+	return sentence.charAt(0).toUpperCase() + sentence.slice(1);
+}	
 
 Phone.prototype.printInfo = function() {
-	var idBrandShortcut = '"#' + this.brandShortcut + '"';
-	console.log(`Telefon marki ${this.brand}, którego kolor jest ${this.color} kosztuje, bagatela ${this.price}$. Chcę, żeby zdjęcie znajdywało sobie div z określonym id - np. ${idBrandShortcut}`);
-//	document.getElementById(idBrandShortcut).innerHTML = '<img src="' + this.img + '" alt="">';
+	var myFirstPrintInfo = `${this.color} ${this.brand} kosztuje ${this.price}$.`;
+	console.log(capitaliseFirstLetterSentence(myFirstPrintInfo));
+	document.getElementById(this.idBrandShortcut).innerHTML = '<img src="' + this.img + '" alt="">';
 };
 
 
-
 var samsung = new Phone("Samsung Galaxy S6", 1250, "brązowy", "https://cdn2.techadvisor.co.uk/cmsdata/features/3596185/Samsung_Galaxy_S6_Edge_2_800.jpg", "samsung");
-var iPhone6s = new Phone("Apple", 2750, "czekoladka", "https://cdn2.techadvisor.co.uk/cmsdata/features/3596185/Samsung_Galaxy_S6_Edge_2_800.jpg", "apple");
-var onePlus = new Phone("chyba Huawei albo inny czort", 2050, "czarny", "https://cdn2.techadvisor.co.uk/cmsdata/features/3596185/Samsung_Galaxy_S6_Edge_2_800.jpg", "one-plus");
+var iPhone6s = new Phone("apple", 2750, "różowy", "https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/r/ef/refurb/iphone6s/refurb-iphone6s-rosegold?wid=1000&hei=1000&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1476208848115", "apple");
+var onePlus = new Phone("huawei", 2050, "czarny", "http://img.c.huaweistatic.com/content/dam/huawei-cbg-site/en/mkt/pdp/phones/p9lite/assets/matebook/images/index-img/sec9/phone2.png", "one-plus");
 
+
+	
 samsung.printInfo();
 iPhone6s.printInfo();
 onePlus.printInfo();
 
-
-Phone.prototype.discountedPrice = function(discountValue) {
+//				apply Discount
+Phone.prototype.applyDiscount = function(discountValue) {
 	return this.price - discountValue;
 };
-
 Phone.prototype.printInfoDiscountedPrice = function() {
-	console.log(`Telefon ${this.brand} po obniżce kosztuje ${this.discountedPrice(100)}$`);
+	var mySecondPrintInfo = `${this.brand} po obniżce kosztuje ${this.applyDiscount(100)}$`;
+	console.log(capitaliseFirstLetterSentence(mySecondPrintInfo));
 };
 
 console.log('___________A TERAZ OBNIŻKA___________');
@@ -60,68 +62,3 @@ samsung.printInfoDiscountedPrice();
 iPhone6s.printInfoDiscountedPrice();
 onePlus.printInfoDiscountedPrice();
 
-
-
-/*
-
-Konstrukcja
-Zaczniemy od stworzenia funkcji konstruującej nasze telefony (klasy) - jako argumenty podaj markę, cenę oraz kolor.
-Aby stworzyć taką funkcję, deklarujemy:
-
-function Phone(brand, price, color) {
-    //Here we put interior of the constructing function.
-}
-Twoja "klasa" (czyli nasza forma do tworzenia nowych telefonów) powinna posiadać następujące parametry: brand, price, color.
-Aby dodać te parametry, musimy skorzystać z this w następujący sposób:
-
-function Phone(brand, price, color) {
-	this.brand = brand;
-	//by using this, the "brand" property of the object we create will assume the value of the brand argument
-	this.price = price;
-	this.color = color;
-}
-
-Funkcje telefonu
-Teraz pora na utworzenie metody - 
-korzystając z prototypu, utwórz dla klasy Phone metodę o nazwie printInfo opisującą telefon za pomocą console.log().
-
-Zdanie skonstruowane przez tę metodę powinno wyglądać mniej więcej tak - 
-"The phone brand is (marka), color is (kolor) and the price is (cena).".
-
-Aby to zrobić, tuż pod swoją funkcją konstruującą zadeklaruj następującą funkcję prototypową:
-
-Phone.prototype.printInfo = function() {
-		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
-}
-
-Produkcja telefonów
-Klasa gotowa - efekt naszej dotychczasowej pracy powinien wyglądać następująco:
-
-function Phone(brand, price, color) {
-	this.brand = brand;
-	this.price = price;
-	this.color = color;
-}
-Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
-}
-Teraz na jej podstawie utwórz obiekty, które będą przedstawiać następujące modele telefonów:
-
-Samsung Galaxy S6
-iPhone 6s
-OnePlus One.
-P
-rzykładowo utworzenie obiektu dla iPhone 6s na podstawie naszej funkcji konstruującej będzie wyglądało następująco:
-
-var iPhone6S = new Phone("Apple", 2250, "silver");
-Teraz przedstawimy w konsoli informacje na temat każdego utworzonego przed chwilą obiektu za pomocą metody printInfo.
-
-Dla naszego nowo utworzonego obiektu iPhone6s, wywołanie metody przedstawiającej telefon dla naszego nowo utworzonego obiektu iPhone6s będzie wyglądało następująco:
-
-iPhone6S.printInfo();
-Gotowe! :)
-
-Rozbuduj swoje telefony o dowolne właściwości. W ramach ćwiczenia możesz stworzyć im też dodatkowe funkcjonalności za pomocą prototypów.
-
-
-*/
